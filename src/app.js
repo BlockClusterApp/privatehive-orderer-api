52844a8eb7850fcfcf51ccef4b601b199ee760d3
Narayan Prusty <arsenal.narayan@gmail.com>
2019-03-10 16:46:31 -0700

    Init

--- src/app.js
+++ src/app.js
@@ -4,146 +4,84 @@ const fs = require('fs')
 const app = require('express')()
 
 const orgName = toPascalCase(process.env.ORG_NAME)
-const mode = process.env.MODE || 'orderer'
 const shareFileDir = process.env.SHARE_FILE_DIR || './crypto' //Make it /etc/hyperledger during deployment
 const workerNodeIP = process.env.WORKER_NODE_IP || '127.0.0.1'
 const ordererPort = process.env.ORDERER_PORT || 7050
+const peerOrgName = toPascalCase(process.env.PEER_ORG_NAME)
+const peerOrgAdminCert = process.env.PEER_ORG_ADMIN_CERT
+const peerOrgCACert = process.env.PEER_ORG_CA_CERT
 
 if(!fs.existsSync(shareFileDir + "initCompleted")) {
-  if(mode === 'orderer') {
-    const cryptoConfigYaml = `
-      OrdererOrgs:
-      - Name: ${orgName}
-        Domain: orderer.${orgName.toLowerCase()}.com
-      PeerOrgs:
-      - Name: ${orgName}
-        Domain: peer.${orgName.toLowerCase()}.com
-        Template:
-          Count: 1
-        Users:
-          Count: 1
-    `
-  
-    shell.mkdir('-p', shareFileDir)
-    shell.cd(shareFileDir)
-    fs.writeFileSync('./crypto-config.yaml', cryptoConfigYaml)
-    shell.exec('cryptogen generate --config=./crypto-config.yaml')
-  
-    const configTxYaml = `
-      Organizations:
-        - &${orgName}Orderer 
-          Name: ${orgName}Orderer
-          ID: ${orgName}Orderer
-          MSPDir: crypto-config/ordererOrganizations/orderer.${orgName.toLowerCase()}.com/msp
-        - &${orgName}
-          Name: ${orgName}
-          ID: ${orgName}
-          MSPDir: crypto-config/peerOrganizations/peer.${orgName.toLowerCase()}.com/msp
-    
-      Channel: &ChannelDefaults
-        Policies:
-          Readers:
-            Type: ImplicitMeta
-            Rule: "ANY Readers"
-          Writers:
-            Type: ImplicitMeta
-            Rule: "ANY Writers"
-          Admins:
-            Type: ImplicitMeta
-            Rule: "ANY Admins"
+  const cryptoConfigYaml = `
+    OrdererOrgs:
+    - Name: ${orgName}
+      Domain: orderer.${orgName.toLowerCase()}.com
+  `
 
-      Profiles:
-        OneOrgGenesis:
-          <<: *ChannelDefaults
-          Orderer:
-            OrdererType: solo
-            Addresses:
-                - ${workerNodeIP}:${ordererPort}
-            BatchTimeout: 2s
-            BatchSize:
-                MaxMessageCount: 10
-                AbsoluteMaxBytes: 98 MB
-                PreferredMaxBytes: 512 KB
+  shell.mkdir('-p', shareFileDir)
+  shell.cd(shareFileDir)
+  fs.writeFileSync('./crypto-config.yaml', cryptoConfigYaml)
+  shell.exec('cryptogen generate --config=./crypto-config.yaml')
+
+  shell.mkdir('-p', `crypto-config/peerOrganizations/peer.${peerOrgName.toLowerCase()}.com/msp/`)
+  shell.mkdir('-p', `crypto-config/peerOrganizations/peer.${peerOrgName.toLowerCase()}.com/msp/admincerts/`)
+  shell.mkdir('-p', `crypto-config/peerOrganizations/peer.${peerOrgName.toLowerCase()}.com/msp/cacerts/`)
+  fs.writeFileSync(`crypto-config/peerOrganizations/peer.${peerOrgName.toLowerCase()}.com/msp/admincerts/Admin@peer.${peerOrgName.toLowerCase()}.com-cert.pem`, peerOrgAdminCert)
+  fs.writeFileSync(`crypto-config/peerOrganizations/peer.${peerOrgName.toLowerCase()}.com/msp/cacerts/ca.peer.${peerOrgName.toLowerCase()}.com-cert.pem`, peerOrgCACert)
+
+  const configTxYaml = `
+    Organizations:
+      - &${orgName}Orderer 
+        Name: ${orgName}Orderer
+        ID: ${orgName}Orderer
+        MSPDir: crypto-config/ordererOrganizations/orderer.${orgName.toLowerCase()}.com/msp
+      - &${peerOrgName}
+        Name: ${peerOrgName}
+        ID: ${peerOrgName}
+        MSPDir: crypto-config/peerOrganizations/peer.${peerOrgName.toLowerCase()}.com/msp
+  
+    Channel: &ChannelDefaults
+      Policies:
+        Readers:
+          Type: ImplicitMeta
+          Rule: "ANY Readers"
+        Writers:
+          Type: ImplicitMeta
+          Rule: "ANY Writers"
+        Admins:
+          Type: ImplicitMeta
+          Rule: "ANY Admins"
+
+    Profiles:
+      OneOrgGenesis:
+        <<: *ChannelDefaults
+        Orderer:
+          OrdererType: solo
+          Addresses:
+              - ${workerNodeIP}:${ordererPort}
+          BatchTimeout: 2s
+          BatchSize:
+              MaxMessageCount: 10
+              AbsoluteMaxBytes: 98 MB
+              PreferredMaxBytes: 512 KB
+          Organizations:
+            - *${orgName}Orderer
+        Consortiums:
+          SingleMemberConsortium:
+              Organizations:
+                - *${peerOrgName}
+      OneOrgChannel:
+        Consortium: SingleMemberConsortium
+        Application:
             Organizations:
-              - *${orgName}Orderer
-          Consortiums:
-            SingleMemberConsortium:
-                Organizations:
-                  - *${orgName}
-        OneOrgChannel:
-          Consortium: SingleMemberConsortium
-          Application:
-              Organizations:
-                  - *${toPascalCase(orgName)}
+                - *${peerOrgName}
 
-    `
-  
-    fs.writeFileSync('./configtx.yaml', configTxYaml)
-    shell.exec('FABRIC_CFG_PATH=$PWD configtxgen -profile OneOrgGenesis -outputBlock ./genesis.block')
-  
-    let files = fs.readdirSync(`crypto-config/peerOrganizations/peer.${orgName.toLowerCase()}.com/ca/`)
-    files.forEach(fileName => {
-      if(fileName.indexOf('_sk') > -1) {
-        shell.exec(`mv crypto-config/peerOrganizations/peer.${orgName.toLowerCase()}.com/ca/${fileName} crypto-config/peerOrganizations/peer.${orgName.toLowerCase()}.com/ca/privateKey`)
-      }
-    })
-  
-    fs.writeFileSync('./initCompleted', "initCompleted")
-  } else if(mode === 'peer') {
-    const cryptoConfigYaml = `
-    PeerOrgs:
-      - Name: ${toPascalCase(orgName)}
-        Domain: peer.${orgName.toLowerCase()}.com
-        Template:
-          Count: 1 
-        Users:
-          Count: 1
-    `
+  `
 
-    shell.mkdir('-p', shareFileDir)
-    shell.cd(shareFileDir)
-    fs.writeFileSync('./crypto-config.yaml', cryptoConfigYaml)
-    shell.exec('cryptogen generate --config=./crypto-config.yaml')
+  fs.writeFileSync('./configtx.yaml', configTxYaml)
+  shell.exec('FABRIC_CFG_PATH=$PWD configtxgen -profile OneOrgGenesis -outputBlock ./genesis.block')
 
-    const configTxYaml = `
-      Organizations:
-      - &${toPascalCase(orgName)}
-        Name: ${toPascalCase(orgName)}
-        ID: ${toPascalCase(orgName)}
-        MSPDir: crypto-config/peerOrganizations/peer.${orgName.toLowerCase()}.com/msp
-      
-      Profiles:
-        OneOrgChannel:
-          Consortium: SingleMemberConsortium
-          Application:
-              Organizations:
-                  - *${toPascalCase(orgName)}
-
-      Channel: &ChannelDefaults
-        Policies:
-          Readers:
-            Type: ImplicitMeta
-            Rule: "ANY Readers"
-          Writers:
-            Type: ImplicitMeta
-            Rule: "ANY Writers"
-          Admins:
-            Type: ImplicitMeta
-            Rule: "ANY Admins"
-    `
-
-    fs.writeFileSync('./configtx.yaml', configTxYaml)
-    shell.exec(`FABRIC_CFG_PATH=$PWD configtxgen -printOrg ${toPascalCase(orgName)} > ${orgName.toLowerCase()}.json`)
-
-    let files = fs.readdirSync(`crypto-config/peerOrganizations/peer.${orgName.toLowerCase()}.com/ca/`)
-    files.forEach(fileName => {
-      if(fileName.indexOf('_sk') > -1) {
-        shell.exec(`mv crypto-config/peerOrganizations/peer.${orgName.toLowerCase()}.com/ca/${fileName} crypto-config/peerOrganizations/peer.${orgName.toLowerCase()}.com/ca/privateKey`)
-      }
-    })
-
-    fs.writeFileSync('./initCompleted', "initCompleted")
-  }
+  fs.writeFileSync('./initCompleted', "initCompleted")
 }
 
-app.listen(3000, () => console.log('Init script running'))
+app.listen(3000, () => console.log('API Server Running'))
