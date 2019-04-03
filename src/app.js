41b7eaa62f019be9158fc296ee86773d53dc6649
Narayan Prusty <arsenal.narayan@gmail.com>
2019-04-03 14:37:10 -0700

    syntax issue fixed

--- src/app.js
+++ src/app.js
@@ -51,12 +51,12 @@ async function updateStatus() {
 async function checkIfOrdererReachable() {
   if(
     await isPortReachable(7050, {host: 'localhost'}) === true && 
-    await isPortReachable(2181, {host: `zk-${orgName.toLowerCase()}-0.zk-svc-${orgName.toLowerCase()}.${namespace}.svc.cluster.local`} === true) &&
-    await isPortReachable(2181, {host: `zk-${orgName.toLowerCase()}-1.zk-svc-${orgName.toLowerCase()}.${namespace}.svc.cluster.local`} === true) &&
-    await isPortReachable(2181, {host: `zk-${orgName.toLowerCase()}-2.zk-svc-${orgName.toLowerCase()}.${namespace}.svc.cluster.local`} === true) &&
-    await isPortReachable(9093, {host: `kafka-${orgName.toLowerCase()}-0.kafka-svc-${orgName.toLowerCase()}.${namespace}.svc.cluster.local`} === true) &&
-    await isPortReachable(9093, {host: `kafka-${orgName.toLowerCase()}-1.kafka-svc-${orgName.toLowerCase()}.${namespace}.svc.cluster.local`} === true) &&
-    await isPortReachable(9093, {host: `kafka-${orgName.toLowerCase()}-2.kafka-svc-${orgName.toLowerCase()}.${namespace}.svc.cluster.local`} === true)
+    await isPortReachable(2181, {host: `zk-${orgName.toLowerCase()}-0.zk-svc-${orgName.toLowerCase()}.${namespace}.svc.cluster.local`}) === true &&
+    await isPortReachable(2181, {host: `zk-${orgName.toLowerCase()}-1.zk-svc-${orgName.toLowerCase()}.${namespace}.svc.cluster.local`}) === true &&
+    await isPortReachable(2181, {host: `zk-${orgName.toLowerCase()}-2.zk-svc-${orgName.toLowerCase()}.${namespace}.svc.cluster.local`}) === true &&
+    await isPortReachable(9093, {host: `kafka-${orgName.toLowerCase()}-0.kafka-svc-${orgName.toLowerCase()}.${namespace}.svc.cluster.local`}) === true &&
+    await isPortReachable(9093, {host: `kafka-${orgName.toLowerCase()}-1.kafka-svc-${orgName.toLowerCase()}.${namespace}.svc.cluster.local`}) === true &&
+    await isPortReachable(9093, {host: `kafka-${orgName.toLowerCase()}-2.kafka-svc-${orgName.toLowerCase()}.${namespace}.svc.cluster.local`}) === true
   ) {
     await updateStatus()
   } else {
